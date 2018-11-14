a40c731fbd3886fed5939cab8bd9fb6fbc35392d
a.anait <airiian.anait@depositphotos.com>
2018-11-14 16:33:43 +0200

    Scrolling the Home page using UiScrollable class

--- src/test/java/tests/BuyFreeFiles.java
+++ src/test/java/tests/BuyFreeFiles.java
@@ -1,16 +1,21 @@
 package tests;
-
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.apache.tools.ant.taskdefs.Touch;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
+public class BuyFreeFiles extends TestBase {
 
-public class LoginUser extends TestBase {
 
     @Test
-    public void loginWithValidCredentials() {
+    public void loginViaEmailAndBuyFreeFiles(){
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.depositphotos.root:id/fab")));
 
@@ -46,9 +51,19 @@ public class LoginUser extends TestBase {
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.depositphotos.root:id/toolbar")));
 
+        String text = "Free files";
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.depositphotos.root:id/scrollContainer\")).scrollIntoView(new UiSelector().resourceId(\"com.depositphotos.root:id/free_files_label\"))").click();
+
+//
+//        TouchAction swipeUp = new TouchAction(driver);
+//        swipeUp.press(PointOption.point(startX ,yStart));
+//        swipeUp.waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)));
+//        swipeUp.moveTo(PointOption.point(xEnd, yEnd));
+//        swipeUp.release();
+//        swipeUp.perform();
+
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.depositphotos.root:id/free_files_list")));
+
     }
-
-
-
-
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,10 @@
 package tests;
+import io.appium.java_client.MobileDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.MalformedURLException;
@@ -20,20 +22,22 @@ public class TestBase {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "8.00");
-        capabilities.setCapability("deviceName","Samsung SM-G950FD");
+        capabilities.setCapability("deviceName", "Samsung SM-G950FD");
         capabilities.setCapability(MobileCapabilityType.UDID, "ce12171ca2cbab0d04");
         capabilities.setCapability("app", "/Users/user/Downloads/Root-debug.apk");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability(MobileCapabilityType.FULL_RESET, "False");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "1000");
-//        capabilities.setCapability("unicodeKeyboard", "true");
-//        capabilities.setCapability("resetKeyboard", "true");
 
 
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-        driver = new AndroidDriver(remoteUrl,capabilities);
-        wait = new WebDriverWait(driver,100);
+        driver = new AndroidDriver(remoteUrl, capabilities);
+        wait = new WebDriverWait(driver, 100);
         System.out.println("Appium server started successfully");
+
+
+
+
 
     }
 
@@ -43,5 +47,5 @@ public class TestBase {
     }
 
 
-}
 
+}
